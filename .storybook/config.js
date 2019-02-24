23c7f9b0769fc2fd1e039c1fc4e83e9848b4f4be
invader <denis.invader@gmail.com>
2019-02-24 14:19:29 +0300

    add storybook options addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,17 @@
-import { configure } from '@storybook/vue';
+import { addDecorator, configure } from '@storybook/vue';
+import { withOptions } from '@storybook/addon-options';
 
-function loadStories () {
-  require('./stories');
-}
+addDecorator (
+  withOptions({
+    name: 'Vuenime',
+    url: 'https://github.com/denisinvader/vuenime',
+    goFullScreen: false,
+    showLeftPanel: true,
+    showDownPanel: true,
+    showSearchBox: false,
+    downPanelInRight: true,
+    enableShortcuts: false,
+  })
+);
 
-configure(loadStories, module);
+configure(() => require('./stories'), module);
