c712ca2cfe646a1329280309ab4b154069447b6b
invader <denis.invader@gmail.com>
2019-02-24 16:21:00 +0300

    implement animejs animation parameters

--- .storybook/stories/Sandbox.vue
+++ .storybook/stories/Sandbox.vue
@@ -12,9 +12,10 @@ export default {
     return (
       <div>
         <p>
+          <label for="colorInput">Color&nbsp;&nbsp;</label>
           <input
-            type="text"
-            step="1"
+            id="colorInput"
+            type="color"
             value={this.color}
             onChange={e => this.color = e.target.value}
           />
@@ -22,9 +23,41 @@ export default {
 
         <Vuenime
           value={this.color}
+          duration={500}
+          easing="easeInCubic"
           scopedSlots={{
             default: color => (
               <h1 style={{ color: color }}>Text color</h1>
+            ),
+          }}
+        />
+
+        <p>
+          <label for="numberInput">Number&nbsp;&nbsp;</label>
+          <input
+            id="numberInput"
+            type="number"
+            step="1"
+            value={this.count}
+            onChange={e => this.count = +e.target.value}
+          />
+        </p>
+
+        <Vuenime
+          value={this.count}
+          duration={2000}
+          easing="steps(10)"
+          round={1}
+          scopedSlots={{
+            default: count => (
+              <div style={{
+                height: '40px',
+                lineHeight: '40px',
+                color: '#ffffff',
+                backgroundColor: 'darkred',
+                padding: '0 10px',
+                width: `${count}px`,
+              }}>{count}</div>
             ),
           }}
         />
--- src/Vuenime.vue
+++ src/Vuenime.vue
@@ -7,17 +7,63 @@ export default {
       type: [Number, String],
       required: true,
     },
+
+    duration: {
+      type: Number,
+      default: 1000,
+    },
+    delay: {
+      type: Number,
+      default: 0,
+    },
+    endDelay: {
+      type: Number,
+      default: 0,
+    },
+    easing: {
+      type: String,
+      default: 'easeOutElastic(1, .5)',
+    },
+    round: {
+      type: Number,
+      default: 0,
+    },
+    direction: {
+      type: String,
+      default: 'normal',
+      validator: value => ['normal', 'reverse', 'alternate'].includes(value),
+    },
+    loop: {
+      type: [Number, Boolean],
+      default: false,
+    },
   },
   data () {
     return {
       target: this.value,
     };
   },
+  computed: {
+    animationParameters () {
+      const {
+        duration,
+        delay,
+        endDelay,
+        easing,
+        round,
+        direction,
+        loop,
+      } = this;
+
+      return { duration, delay, endDelay, easing, round, direction, loop };
+    },
+  },
   watch: {
     value () {
       anime({
         targets: this,
         target: this.value,
+        ...this.animationParameters,
       });
     },
   },
