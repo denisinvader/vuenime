9391ea29843801f11d6424d8d8cde25160ddda0d
invader <denis.invader@gmail.com>
2019-02-26 01:09:31 +0300

    try this

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import _vuenime from './Vuenime.vue';
-
-export const Vuenime = _vuenime.default || _vuenime;
-
-export default {
-  install (Vue) {
-    Vue.component('vuenime', Vuenime);
-  },
-};
--- src/utils.js
+++
@@ -1,1 +0,0 @@
-export const isArray = obj => Array.isArray(obj);
